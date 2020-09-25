805a35aa8f80df1ac4f6fb18b83e31c4b4a26769
Philip <p208p2002@gmail.com>
2020-09-25 08:06:52 +0000

    init script

--- init_dataset.sh
+++ init_dataset.sh
@@ -1,3 +1,4 @@
 gdown -O dataset.zip --id 19Gz1Cr9iChxOsWdm5cdpUWYqfaxgKStH
 unzip dataset.zip -d prepro_data
-mv prepro_data/DocRED_baseline_metadata/* prepro_data
+mv prepro_data/ data
+mv data/DocRED_baseline_metadata/ code/prepro_data/
